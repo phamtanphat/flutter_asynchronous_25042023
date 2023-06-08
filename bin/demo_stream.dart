dfca12ab97cb3c7efb6f87bc708f815eb9428349
phamtanphat <phatdroid94@gmail.com>
2023-06-08 18:32:28 +0700

    streamcontroller

--- bin/demo_stream.dart
+++ bin/demo_stream.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 void main() {
   // Stream
   // 1: Tạo từ Future
@@ -14,11 +16,31 @@ void main() {
   // });
 
   // 2: Tạo từ Iterable
-  Iterable<int> iterable = Iterable.generate(10, (index) => index + 1);
-  Stream<int> streamIterable = Stream.fromIterable(iterable);
+  // Iterable<int> iterable = Iterable.generate(10, (index) => index + 1);
+  // Stream<int> streamIterable = Stream.fromIterable(iterable);
+  //
+  // int index = 0;
+  // streamIterable.listen((number) {
+  //   print("$number - ${index++}");
+  // });
 
-  int index = 0;
-  streamIterable.listen((number) {
-    print("$number - ${index++}");
+  // 3: Tạo ra từ periodic
+
+  // Stream<int> streamPeriodic = Stream.periodic(Duration(seconds: 1), (computation){
+  //   throw Exception("Lỗi");
+  // });
+  //
+  // streamPeriodic
+  //     .listen((event) => print(event))
+  //     .onError((error) => print("Lỗi $error"));
+
+  // StreamController
+
+  StreamController<String> stringController = StreamController();
+
+  stringController.stream.listen((event) => print(event));
+
+  Future.delayed(Duration(seconds: 2), () {
+    stringController.sink.add("Tèo");
   });
 }
